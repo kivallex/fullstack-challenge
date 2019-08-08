c4f7b8ee98c5cdf0f718d4338402cda4bfbdb331
Kenneth Suh <kenneth.y.suh@gmail.com>
2019-08-07 23:16:01 -0700

    add hackaday projects endpoint calls

--- api.js
+++ api.js
@@ -1,25 +1,13 @@
 // load the things we need
 var express = require("express");
 var path = require("path");
+var axios = require("axios");
 var app = express();
 
 // temporary solution
 var hackadayAPIKey = "SD3xfuN4z7CcduPI";
-
-// fake project information
-var projects = [
-  { name: "Project #", projectNumber: 1, projectDetail: "random" },
-  { name: "Project #", projectNumber: 2, projectDetail: "random" },
-  { name: "Project #", projectNumber: 3, projectDetail: "random" },
-  { name: "Project #", projectNumber: 4, projectDetail: "random" },
-  { name: "Project #", projectNumber: 5, projectDetail: "random" },
-  { name: "Project #", projectNumber: 6, projectDetail: "random" },
-  { name: "Project #", projectNumber: 7, projectDetail: "random" },
-  { name: "Project #", projectNumber: 8, projectDetail: "random" },
-  { name: "Project #", projectNumber: 9, projectDetail: "random" },
-  { name: "Project #", projectNumber: 10, projectDetail: "random" },
-  { name: "Project #", projectNumber: 11, projectDetail: "random" }
-];
+var PAGE_SIZE = 10;
+var LOAD_SIZE = 11;
 
 // set the view engine to ejs
 app.set("view engine", "ejs");
@@ -28,33 +16,67 @@ app.use(express.static(path.join(__dirname, "views")));
 // use res.render to load up an ejs view file
 
 // index page
-app.get("/", (req, res) => {
+app.get("/", async function(_, res) {
+  var response = await axios.get(
+    "http://api.hackaday.io/v1/projects?api_key=" +
+      hackadayAPIKey +
+      "&per_page=" +
+      LOAD_SIZE
+  );
+  var projects = response.data.projects;
+
+  response = await axios.get(
+    "http://api.hackaday.io/v1/users/" +
+      projects.owner_id +
+      "?api_key" +
+      hackadayAPIKey
+  );
+
   res.render("pages/projectList", {
-    projects: projects
+    projects: projects.slice(0, PAGE_SIZE),
+    hasNext: projects.length === LOAD_SIZE
   });
 });
 
 // to get data for next/prev page
-app.get("/projects", (req, res) => {
+app.get("/projects", function(req, res) {
   // pagintation
-  // sent from front-end
+  // sent from front-end, decremented since array starts at 0
   var page = req.query.page;
   page--;
 
-  // page = 2
-  const range = projects.slice(page * 6, page * 6 + 6);
+  axios
+    .get(
+      "http://api.hackaday.io/v1/projects?api_key=" +
+        hackadayAPIKey +
+        "&page=" +
+        page +
+        "&per_page=" +
+        LOAD_SIZE
+    )
+    .then(function(response) {
+      // page = 2
+      var projects = response.data.projects;
 
-  res.json({
-    projects: range
-  });
+      res.json({
+        projects: projects.slice(0, PAGE_SIZE),
+        hasNext: projects.length === LOAD_SIZE
+      });
+    });
 });
 
 // detail page
-app.get("/detail/:projectNumber", (req, res) => {
-  // find the certain project from projects
-  var project = projects.filter(project => {
-    return project.projectNumber == req.params.projectNumber;
-  })[0];
+app.get("/detail/:id", function(req, res) {
+  axios
+    .get(
+      "http://api.hackaday.io/v1/projects/" +
+        req.params.id +
+        "?api_key=" +
+        hackadayAPIKey
+    )
+    .then(function(response) {
+      var projects = response.data.projects;
+    });
 
   res.render("pages/projectDetail", {
     name: project.name,
--- views/js/projectList.js
+++ views/js/projectList.js
@@ -1,75 +1,30 @@
 // when user clicks 'Next' button show the next items
-console.log("load");
-
 function next() {
   // expecting url of http://localhost:3000?page=2
   // if no page, it means page 1
-  var pageNum = getParameter("page");
+  var pageNum = Number(getParameter("page"));
 
   if (pageNum === null) {
     pageNum = 1;
   }
+  if (isNaN(pageNum)) {
+    return;
+  }
 
-  pageNum++;
-
-  fetch("/projects?page=" + pageNum)
-    .then(function(response) {
-      return response.json();
-    })
-    .then(function(data) {
-      var projects = data.projects;
-
-      // if (endVal < projects.length) {
-      //   startVal = startVal + 6; // when you press next button it should show the next 6 items
-      //   endVal = endVal + 6;
-      // }
-
-      const projectsGrid = document.getElementById("project-grid");
-      // empty the div
-      projectsGrid.innerHTML = "";
-
-      console.log(projects);
-
-      for (var i = 0; i < projects.length; i++) {
-        // <div class="col-sm-4">
-        //   <a href="/detail/<%= projects[start].projectNumber %>">
-        //     <h4>
-        //       <%= projects[start].name %><%= projects[start].projectNumber %>
-        //     </h4>
-        //     <p><%= projects[start].projectDetail %></p>
-        //   </a>
-        // </div>
-
-        var project = document.createElement("div");
-        project.classList.add("col-sm-4");
-
-        var projectLink = document.createElement("a");
-        projectLink.href = "/detail/" + projects[i].projectNumber;
-
-        var projectHeader = document.createElement("h4");
-        projectHeader.innerHTML = projects[i].name + projects[i].projectNumber;
-
-        var projectP = document.createElement("p");
-        projectP.innerHTML = projects[i].projectDetail;
-
-        projectLink.appendChild(projectHeader);
-        projectLink.appendChild(projectP);
-        project.appendChild(projectLink);
-        projectsGrid.appendChild(project);
-      }
-
-      // creating unique url
-      window.history.pushState({}, null, "?page=" + pageNum);
-    });
+  nextPrevHandler(pageNum + 1);
 }
 
-function prev(startVal, endVal) {
-  if (startVal >= 6) {
-    // if 'i' is greater than 5 (meaning it is displaying items #7-12)
-    // then you move back 6
-    startVal = startVal - 6;
-    endVal = endVal - 6;
+function prev() {
+  var pageNum = Number(getParameter("page"));
+
+  if (pageNum === null) {
+    pageNum = 1;
   }
+  if (isNaN(pageNum) || pageNum <= 1) {
+    return;
+  }
+
+  nextPrevHandler(pageNum - 1);
 }
 
 function getParameter(paramName) {
@@ -87,29 +42,49 @@ function getParameter(paramName) {
   return null;
 }
 
-// var splitView = [];
-// var start = 0, end = 6;
-// // make this a loop so all these function as one
-// splitView.push(projects.slice(start, end));
+function nextPrevHandler(pageNum) {
+  return fetch("/projects?page=" + pageNum)
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(data) {
+      var projects = data.projects;
+      var projectsGrid = document.getElementById("project-grid");
+      // empty the div
+      projectsGrid.innerHTML = "";
 
-// while(start < end) {
-//   // display project name, number, and detail
-//   // start ++;
-// };
+      for (var i = 0; i < projects.length; i++) {
+        var project = document.createElement("div");
+        project.classList.add("col-sm-4");
 
-// // dont display previous button if start === 0
-// // else previous button will show and function as
-// prev(){
-//   start -= 6;
-//   end -= 6;
+        var projectLink = document.createElement("a");
+        projectLink.href = "/detail/" + projects[i].id;
 
-//   splitView.push(projects.slice(start, end));
-// };
+        var projectHeader = document.createElement("h4");
+        projectHeader.innerHTML = projects[i].name;
 
-// // dont display next button if end >= projects.length
-// next(){
-//   start += 6;
-//   end += 6;
+        projectLink.appendChild(projectHeader);
+        project.appendChild(projectLink);
+        projectsGrid.appendChild(project);
+      }
 
-//   splitView.push(projects.slice(start, end));
-// };
+      // creating unique url
+      window.history.pushState({}, null, "?page=" + pageNum);
+
+      // hide relevant button
+      var prev = document.getElementById("prev");
+      var next = document.getElementById("next");
+
+      if (pageNum <= 1) {
+        prev.classList.add("hide");
+      } else {
+        prev.classList.remove("hide");
+      }
+
+      if (!data.hasNext) {
+        next.classList.add("hide");
+      } else {
+        next.classList.remove("hide");
+      }
+    });
+}
